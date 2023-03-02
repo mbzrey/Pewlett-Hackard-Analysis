8541ca3b5e84fe8cec7a775862b8db5832551375
unknown <michele.beltran@gmail.com>
2023-03-02 17:21:36 -0600

    Added queries.sql file and retirement_info table

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- Creating queries for employees born between two dates
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+-- Query for retiremente eligibility
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+
+-- Number of employees eligible for retirement
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Creating new table of eligible employees
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+SELECT * FROM retirement_info;
